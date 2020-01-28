7ba1b3875fd48ba7ed213c0bb4c62b4b5f8ec30e
GCdM <59828466+GCdM@users.noreply.github.com>
2020-01-28 15:50:35 +0000

    Upgraded to HTTPS, using self-signed certs for localhost

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 
 const app = require('express')()
-const server = require('http').Server(app)
-const io = require('socket.io')(server)
+const https = require('https')
+const fs = require('fs')
+// const io = require('socket.io')(server)
 const mongoose = require('mongoose')
 
 // const session = require('./src/session')
@@ -11,11 +12,23 @@ const UserSchema = require('./src/db/UserSchema')
 const ConversationSchema = require('./src/db/ConversationSchema')
 const MessageSchema = require('./src/db/MessageSchema')
 
+///// CONFIGURE SSL/TLS /////
+const options = {
+  key: fs.readFileSync("./ssl/localhost.key"),
+  // key: fs.readFileSync("/srv/www/keys/my-site-key.pem"),
+  cert: fs.readFileSync("./ssl/localhost.crt"),
+  // cert: fs.readFileSync("/srv/www/keys/chain.pem"),
+}
+
+const server = https.createServer(options, app)
+
+///// SPIN UP SERVER /////
+//////////////////////////
+///// Handle database connection error
 db.on('error', (err) => {
   console.log('MongoDB Connection Error: ', err)
 })
 
-///// SPIN UP SERVER /////
 ///// Check that the databse connected succesfully
 db.once('open', () => {
   console.log('MongoDB Connected!')
@@ -32,14 +45,30 @@ db.once('open', () => {
 })
 
 ///// MIDDLEWARES /////
+///////////////////////
+///// Set headers for CORS policy
+// app.use((req, res, next) => {
+//   debugger
+//   res.header('Access-Control-Allow-Origin', '*')  
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+//   res.header('Access-Control-Allow-Credentials', 'true')
+
+//   next()
+// })
 
 ///// HTTP ROUTES /////
+///////////////////////
+app.get('/status', (req, res) => {
+  console.log("==> GET to /status")
+
+  res.status(200).send({ status: "Up and running" })
+})
 
 app.post('/login', (req, res) => {
-  console.log("POST to /login")
+  console.log("==> POST to /login")
   debugger
 
-  const userData = { _id: 0, username: "Wrong" }
+  const userData = { _id: 0, username: "Fake" }
   
   res.status(200).send(userData)
 })
